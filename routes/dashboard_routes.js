8f09bbdd1978b44ce5ea69d90729e64543670971
ramadlana <hidayahweb@gmail.com>
2022-05-01 23:29:48 +0700

    add day js and move to dashboar for  transaction

--- routes/dashboard_routes.js
+++ routes/dashboard_routes.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const permission = require("../middleware/permission");
 const router = express.Router();
+const Joi = require("joi");
+const midtransClient = require("midtrans-client");
+const sha = require("js-sha512");
 
-const { PrismaClient } = require("@prisma/client");
+const { PrismaClient, Prisma } = require("@prisma/client");
+const dayjs = require("dayjs");
 const prisma = new PrismaClient();
 
 router.get("/", async (req, res) => {
@@ -114,6 +118,106 @@ router.get("/alluser-cursor", async (req, res) => {
 });
 
 // PROD // PROD // PROD // PROD // PROD // PROD // PROD // PROD
+// APP midtrans handle Checkout
+router.get("/checkout", async (req, res) => {
+  // Get id from params
+  const { id } = req.query;
+
+  // Get user attributes from radcheck table
+  const user = await prisma.radcheck.findUnique({
+    where: { id: parseInt(id) },
+    include: { app_service: true },
+  });
+
+  if (!user) return res.send({ error: "user not found" });
+
+  // Get gross_ammount, status_code, and order_id in order to generate signature key in transaction table
+  const gross_amount = user.app_service.service_ammount;
+  const status_code = 200;
+  const serverkey = process.env.SERVER_KEY_MIDTRANS;
+
+  let order_id;
+
+  // Create app_transaction item in order generate order_id
+  try {
+    const transaction = await prisma.app_transaction.create({
+      data: {
+        gross_amount: gross_amount,
+        radcheck_id: parseInt(id),
+        transaction_time: new Date(),
+      },
+    });
+
+    order_id = transaction.order_id;
+  } catch (error) {
+    return res.send({
+      error: "error when creating transaction",
+      detail: error.message,
+    });
+  }
+
+  // Generate signature key
+  const signature_key = sha.sha512(
+    // convert orderId to string, and add .00 to gross amount because midtrans using .00 after gross ammount, for ex: 50000.00
+    order_id.toString() + status_code + gross_amount + ".00" + serverkey
+  );
+
+  // Update app_transaction
+  try {
+    await prisma.app_transaction.update({
+      where: {
+        order_id: order_id,
+      },
+      data: {
+        signature_key: signature_key,
+        transaction_status: "pending",
+      },
+    });
+  } catch (error) {
+    return res.send({ error: error.message });
+  }
+
+  try {
+    // Create Snap API instance
+    let snap = new midtransClient.Snap({
+      // Set to true if you want Production Environment (accept real transaction).
+      isProduction:
+        process.env.IS_PRODUCTION_MIDTRANS === "false" ? false : true,
+      serverKey: process.env.SERVER_KEY_MIDTRANS,
+    });
+
+    let parameter = {
+      transaction_details: {
+        order_id: order_id,
+        gross_amount: gross_amount,
+      },
+      credit_card: {
+        secure: true,
+      },
+      customer_details: {
+        first_name: user.first_name,
+        last_name: user.last_name,
+        email: user.email,
+        phone: user.phone,
+      },
+    };
+    const transaction = await snap.createTransaction(parameter);
+    const transactionToken = await transaction.token;
+    res.send({
+      transactionToken: transactionToken,
+      transactionDetail: transaction,
+    });
+  } catch (error) {
+    res.send({
+      error: "failed when creating payment gateway process",
+      error_messages: error.ApiResponse
+        ? error.ApiResponse.error_messages[0]
+        : JSON.stringify(error),
+    });
+  }
+});
+
+// Get all radius user
 router.get("/all-radius-user", async (req, res) => {
   let { maxPerpage, page, searchBy, searchString, sortBy, sortMethod } =
     req.query;
@@ -171,4 +275,143 @@ router.get("/all-radius-user", async (req, res) => {
   }
 });
 
+// Get single radius user and detail
+router.get("/radiususer/:id", async (req, res) => {
+  const { id } = req.params;
+  const user = await prisma.radcheck.findUnique({
+    where: { id: parseInt(id) },
+    include: { app_service: true, radusergroup: true },
+  });
+  res.send({ user: user });
+});
+
+// Add new radius user
+router.post("/radiususer", async (req, res) => {
+  const formSchema = Joi.object({
+    username: Joi.string().required(),
+    first_name: Joi.string().required(),
+    last_name: Joi.string().required(),
+    address: Joi.string().required(),
+    phone: Joi.string().required(),
+    service: Joi.string().required(),
+    email: Joi.string().email().required(),
+    password: Joi.string().min(4).required(),
+    retype_password: Joi.string().min(4).required(),
+  });
+  // Joi Validate
+  const formValidate = formSchema.validate(req.body.data);
+  // Joi Error
+  if (formValidate.error)
+    return res
+      .status(401)
+      .send({ message: formValidate.error.details[0].message });
+  const {
+    username,
+    first_name,
+    last_name,
+    address,
+    phone,
+    service,
+    email,
+    password,
+  } = req.body.data;
+  try {
+    const new_radius_user = await prisma.radcheck.create({
+      data: {
+        username: username,
+        // Convert Password key to Value. Because Freeradius using value key instead password
+        value: password,
+        // expirydate: dayjs().add(30, "day").format(),
+        expirydate: dayjs("2000-01-01T07:00:00").format("YYYY-MM-DDTHH:mm:ssZ"),
+        email: email,
+        address: address,
+        first_name: first_name,
+        last_name: last_name,
+        phone: phone,
+        service_status: "registered",
+        app_service: { connect: { service_name: service } },
+        radusergroup: {
+          create: { username: username, groupname: "registered" },
+        },
+      },
+    });
+    return res.send(new_radius_user);
+  } catch (e) {
+    if (e instanceof Prisma.PrismaClientKnownRequestError) {
+      // The .code property can be accessed in a type-safe manner
+      if (e.code === "P2002") {
+        return res.status(401).send({ message: "Username must be unique" });
+      }
+    }
+    throw e;
+  }
+});
+
+// Edit radius user
+router.patch("/radiususer", async (req, res) => {
+  const { editedData } = req.body;
+  const formSchema = Joi.object({
+    id: Joi.number().required(),
+    username: Joi.string().required(),
+    first_name: Joi.string().required(),
+    last_name: Joi.string().required(),
+    address: Joi.string().required(),
+    phone: Joi.string().required(),
+    email: Joi.string().email().required(),
+  });
+  // Joi Validate
+  const formValidate = formSchema.validate(editedData);
+  // Joi Error
+  if (formValidate.error)
+    return res
+      .status(401)
+      .send({ message: formValidate.error.details[0].message });
+  const { id, username, first_name, last_name, address, phone, email } =
+    editedData;
+  try {
+    const check_status = await prisma.radcheck.findUnique({
+      where: { id: id },
+      include: { radusergroup: true },
+    });
+
+    await prisma.radcheck.update({
+      where: { id: id },
+      data: {
+        username: username,
+        email: email,
+        address: address,
+        first_name: first_name,
+        last_name: last_name,
+        phone: phone,
+      },
+    });
+    return res.send({ message: "User info updated" });
+  } catch (e) {
+    if (e instanceof Prisma.PrismaClientKnownRequestError) {
+      // The .code property can be accessed in a type-safe manner
+      if (e.code === "P2002") {
+        return res.status(401).send({ message: "Username must be unique" });
+      }
+    }
+    throw e;
+  }
+});
+
+// get services list
+router.get("/getservices", async (req, res) => {
+  try {
+    const services = await prisma.app_service.findMany({
+      where: {
+        NOT: [
+          { service_name: "registered" },
+          { service_name: "suspend" },
+          { service_name: "terminated" },
+        ],
+      },
+    });
+    return res.send({ data: services });
+  } catch (error) {
+    console.log(error);
+  }
+});
 module.exports = router;
--- routes/main_routes.js
+++ routes/main_routes.js
@@ -1,121 +1,90 @@
 const express = require("express");
 const router = express.Router();
 
+// Dayjs Import
+const dayjs = require("dayjs");
+let utc = require("dayjs/plugin/utc");
+let timezone = require("dayjs/plugin/timezone"); // dependent on utc plugin
+dayjs.extend(utc);
+dayjs.extend(timezone);
+
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
-const sha = require("js-sha512");
+
 // Start routes
 
 // Home
 router.get("/", async (req, res) => {
-  return res.status(200).send({ message: "this is home page" });
-});
-
-// Mid Trans Checkout
-router.get("/checkout", async (req, res) => {
-  const { order_id } = req.query;
-
-  const gross_amount = "50000.00";
-  const status_code = 200;
-  // const gross_amount = 10000;
-  const serverkey = "SB-Mid-server-fy6I1u7MkJ2HC8EU8WFwPMBr";
-
-  const midtransClient = require("midtrans-client");
-
-  // Create Snap API instance
-  let snap = new midtransClient.Snap({
-    // Set to true if you want Production Environment (accept real transaction).
-    isProduction: false,
-    serverKey: "SB-Mid-server-fy6I1u7MkJ2HC8EU8WFwPMBr",
-  });
-
-  let parameter = {
-    transaction_details: {
-      order_id: order_id,
-      gross_amount: gross_amount,
-    },
-    credit_card: {
-      secure: true,
-    },
-    customer_details: {
-      first_name: "hidayah",
-      last_name: "ramadlana",
-      email: "hidayah.pra@gmail.com",
-      phone: "08111222333",
-    },
-  };
-
-  const signature_key = sha.sha512(
-    order_id + status_code + gross_amount + serverkey
-  );
-
-  // Save to table transaction
-  try {
-    const save = await prisma.app_transaction.create({
-      data: {
-        order_id: order_id,
-        signature_key: signature_key,
-        transaction_status: "pending",
-      },
-    });
-  } catch (error) {
-    res.send({ error: error });
-  }
-
-  try {
-    const transaction = await snap.createTransaction(parameter);
-    const transactionToken = await transaction.token;
-    res.send({
-      transactionToken: transactionToken,
-      transactionDetail: transaction,
-    });
-  } catch (error) {
-    res.send({
-      transaction: "failed",
-      error_messages: error.ApiResponse
-        ? error.ApiResponse.error_messages[0]
-        : JSON.stringify(error),
-    });
-  }
+  return res.status(200).send({ message: "Billing Management System" });
 });
 
 // Midtrans POST payment handle -> If payment success enable radius account
 router.post("/handle-payment", async (req, res) => {
   // get data from req.body HOOK midtrans
   const {
+    va_numbers,
+    transaction_id,
+    settlement_time,
+    payment_type,
+    payment_amounts,
     status_code,
+    status_message,
+    merchant_id,
+    fraud_status,
     order_id,
-    gross_amount,
     signature_key,
     transaction_status,
+    currency,
   } = req.body;
+
+  if (!signature_key)
+    return res.status(403).send({ message: "you have no access" });
+
+  const settlement_time_formatted = new Date(settlement_time);
 
   // get signature_key from app_transaction
   const app_transaction_db = await prisma.app_transaction.findUnique({
     where: {
-      order_id: order_id,
+      order_id: parseInt(order_id),
     },
   });
 
   // if generated signature key from checkout match with signature key from midtrans
   if (app_transaction_db.signature_key === signature_key) {
     // do logic if payment success here
-    await prisma.app_transaction.update({
-      where: { order_id: order_id },
-      data: {
-        status_code: status_code,
-        gross_amount: gross_amount,
-        transaction_status: transaction_status,
-      },
-    });
-    return res.send({ message: "success" });
-  }
 
+    // Save to DB
+    try {
+      await prisma.app_transaction.update({
+        where: { order_id: parseInt(order_id) },
+        data: {
+          // update transaction status
+          transaction_status: transaction_status,
+          va_numbers: va_numbers,
+          payment_amounts: payment_amounts,
+          transaction_id: transaction_id,
+          settlement_time: settlement_time_formatted,
+          payment_type: payment_type,
+          status_code: status_code,
+          status_message: status_message,
+          merchant_id: merchant_id,
+          fraud_status: fraud_status,
+          currency: currency,
+        },
+      });
+      return res.send({ message: "success" });
+    } catch (error) {
+      return res.send({ error: error.message ? error.message : error });
+    }
+  }
   return res.send({ message: "error" });
 });
 
 router.get("/payment-success", async (req, res) => {
-  return res.send({ message: "horay.. pembayaran berhasil" });
+  const { order_id, transaction_status } = req.query;
+  return res.send({
+    message: `horay.. pembayaran berhasil status pembayaran kamu dengan order ID: ${order_id} masih ${transaction_status} ketika sudah sukses kami akan mengirimkan notifikasi`,
+  });
 });
 
 router.get("/payment-failed", async (req, res) => {
